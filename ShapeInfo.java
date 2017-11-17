de08acf527cf851bddbeeca8ac47b43daa3935ce
sherwyn <sherwyn.sen@gmail.com>
2017-11-16 20:44:36 -0800

    wrote the first base class

---
+++ ShapeInfo.java
@@ -0,0 +1,26 @@
+class ShapeInfo {
+    private int x;
+    private int y;
+
+    public Info(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int setX(int x) {
+        this.x = x;
+    }
+
+    public int setY(int y) {
+        this.y = y;
+    }
+
+}
