5c9094400c56f8b4ed05e7c9185e42173aa145f0
sherwyn <sherwyn.sen@gmail.com>
2017-12-06 18:37:33 -0800

    cleaned up main whiteboard class, separated main GUI elements into three parts

---
+++ src/ControlPanel.java
@@ -0,0 +1,24 @@
+public class ControlPanel extends JPanel {
+   public ControlPanel() {
+		//Add buttons section
+		/*JPanel addButtons = new JPanel();
+		addButtons.setBounds(0, 0, 500, 30);
+		addButtons.setBackground(Color.BLACK);
+		addButtons.setLayout(new FlowLayout());
+		addButtons.setVisible(true);
+		
+		JLabel preButtonAddText = new JLabel("Add");
+		JButton addRect = new JButton("Rect");
+		
+		JButton addOval = new JButton("Oval");
+		JButton addLine = new JButton("Line");
+		JButton addText = new JButton("Text");
+		
+		addButtons.add(preButtonAddText);
+		addButtons.add(addRect);
+		addButtons.add(addOval);
+		addButtons.add(addLine);
+		addButtons.add(addText);
+		whiteboard.add(addButtons);*/
+   }
+}
--- src/Whiteboard.java
+++ src/Whiteboard.java
@@ -7,47 +7,36 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Whiteboard extends JFrame{
+public class Whiteboard extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+    
+    private Canvas canvas;
+    private ControlPanel controlPanel;
+    private StatusTable statusTable;
 
 	public Whiteboard(){
-		this.setTitle("Whiteboard");
-		this.setBackground(Color.GRAY);
-		this.setSize(900, 400);
-		this.setLayout(new BorderLayout());
+        // initialize main window
+		super("whiteboard");
+		this.setLayout(new BorderLayout()); 
+		
+        // initialize each subwindow
+        this.canvas         = new Canvas();
+        this.controlPanel   = new ControlPanel();
+        this.statusTable    = new StatusTable();
+
+        // place each subwindow onto the main GUI
+		this.add(this.canvas, BorderLayout.EAST);
+        this.add(this.controlPanel, BorderLayout.NORTH);
+        this.add(this.statusTable, BorderLayout.SOUTH);
+		 
+        // actually show the window
+        this.pack();
 		this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	public static void main(String[] args){
 		Whiteboard whiteboard = new Whiteboard();
-		Canvas canvas = new Canvas();
-		whiteboard.add(canvas);
-		
-		//Add buttons section
-		JPanel addButtons = new JPanel();
-		addButtons.setBounds(0, 0, 500, 30);
-		addButtons.setBackground(Color.BLACK);
-		addButtons.setLayout(new FlowLayout());
-		addButtons.setVisible(true);
-		
-		JLabel preButtonAddText = new JLabel("Add");
-		JButton addRect = new JButton("Rect");
-		
-		JButton addOval = new JButton("Oval");
-		JButton addLine = new JButton("Line");
-		JButton addText = new JButton("Text");
-		
-		addButtons.add(preButtonAddText);
-		addButtons.add(addRect);
-		addButtons.add(addOval);
-		addButtons.add(addLine);
-		addButtons.add(addText);
-		whiteboard.add(addButtons);
-		
-		
-		
 	}
-	
-	
 }
