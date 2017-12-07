a68e04778ff292b21a1e97d8d059c1bed7b81aa3
Brandon Archbold <babrandon21@gmail.com>
2017-12-06 20:02:33 -0800

    Added control panel buttons

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -1,32 +1,122 @@
 import java.awt.Color;
 import java.awt.FlowLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class ControlPanel extends JPanel {
   
 	private static final long serialVersionUID = 1L;
 
 public ControlPanel() {
-		//Add buttons section
-		this.setLayout(new FlowLayout());
+		
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		this.setVisible(true);
+		
+		//*****Add buttons section*******
+		JPanel addButtons = new JPanel();
+		addButtons.setLayout(new FlowLayout(FlowLayout.LEFT));
+		addButtons.setVisible(true);
 		
 		JLabel preButtonAddText = new JLabel("Add Shapes:");
 		JButton addRect = new JButton("Rect");
-		
 		JButton addOval = new JButton("Oval");
 		JButton addLine = new JButton("Line");
 		addLine.setEnabled(false);
 		JButton addText = new JButton("Text");
 		addText.setEnabled(false);
 		
-		this.add(preButtonAddText);
-		this.add(addRect);
-		this.add(addOval);
-		this.add(addLine);
-		this.add(addText);
+		addButtons.add(preButtonAddText);
+		addButtons.add(addRect);
+		addButtons.add(addOval);
+		addButtons.add(addLine);
+		addButtons.add(addText);
+		
+		this.add(addButtons);
+		//******************************
+		
+		//*****Text editing section*******
+		JPanel editTextPanel = new JPanel();
+		editTextPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		editTextPanel.setVisible(true);
+		
+		JLabel preEditText = new JLabel("Edit Text:");
+		JTextField textEditor = new JTextField();
+		textEditor.setColumns(30);
+		
+		editTextPanel.add(preEditText);
+		editTextPanel.add(textEditor);
+		
+		this.add(editTextPanel);
+		//******************************
+		
+		//*****Shape color section*******
+		JPanel shapeColorPanel = new JPanel();
+		shapeColorPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		shapeColorPanel.setVisible(true);
+		
+		JLabel shapeColorText = new JLabel("Set Shape Color: ");
+		JButton setColorButton = new JButton("Set Color");
+		
+		shapeColorPanel.add(shapeColorText);
+		shapeColorPanel.add(setColorButton);
+		
+		this.add(shapeColorPanel);
+		//******************************
+		
+		//*****Edit shape section*******
+		JPanel editShapePanel = new JPanel();
+		editShapePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		editShapePanel.setVisible(true);
+		
+		JLabel editShapeText = new JLabel("Edit Shapes:");
+		JButton sendToFront = new JButton("Send to Front");
+		JButton sendToBack = new JButton("Send to Back");
+		JButton removeButton = new JButton("Remove this Shape");
+		
+		editShapePanel.add(editShapeText);
+		editShapePanel.add(sendToFront);
+		editShapePanel.add(sendToBack);
+		editShapePanel.add(removeButton);
+		
+		this.add(editShapePanel);
+		//******************************
+		
+		//*****FileIO section*******
+		JPanel fileIOPanel = new JPanel();
+		fileIOPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		fileIOPanel.setVisible(true);
+		
+		JLabel fileIOText = new JLabel("Load/Save Content:");
+		JButton saveButton = new JButton("Save");
+		JButton loadButton = new JButton("Load");
+		JButton saveToPngButton = new JButton("Save as PNG");
+		
+		fileIOPanel.add(fileIOText);
+		fileIOPanel.add(saveButton);
+		fileIOPanel.add(loadButton);
+		fileIOPanel.add(saveToPngButton);
+		
+		this.add(fileIOPanel);
+		//******************************
+		
+		//*****Networking section*******
+		JPanel networkPanel = new JPanel();
+		networkPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		networkPanel.setVisible(true);
+		
+		JLabel networkingLabel = new JLabel("Networking:");
+		JButton startServerButton = new JButton("Start Server");
+		JButton startClientButton = new JButton("Start Client");
+		
+		networkPanel.add(networkingLabel);
+		networkPanel.add(startServerButton);
+		networkPanel.add(startClientButton);
+		
+		this.add(networkPanel);
+		//******************************
    }
 }
