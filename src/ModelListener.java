49e4d8903b386c43da0a7f3477893ff5aeb051c6
Brandon Archbold <babrandon21@gmail.com>
2017-12-07 22:32:07 -0800

    removed implementation of componentListener

--- src/ModelListener.java
+++ src/ModelListener.java
@@ -1,11 +1,9 @@
-import java.awt.event.ComponentListener;
-
 /*
 Interface to listen for shape change notifications.
 The modelChanged() notification includes a pointer to the model that changed. There is not detail about
 what the exact change
 was. */
-public interface ModelListener extends ComponentListener {
+public interface ModelListener {
 	
 	public void modelChanged(Info model); 
 	
