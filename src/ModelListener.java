6cd502a609318aba14fcbb703de27d08d6a00d2f
Brandon Archbold <babrandon21@gmail.com>
2017-12-07 22:30:10 -0800

    extended componentListener

--- src/ModelListener.java
+++ src/ModelListener.java
@@ -1,13 +1,13 @@
+import java.awt.event.ComponentListener;
+
 /*
 Interface to listen for shape change notifications.
 The modelChanged() notification includes a pointer to the model that changed. There is not detail about
 what the exact change
 was. */
-public interface ModelListener {
+public interface ModelListener extends ComponentListener {
 	
 	public void modelChanged(Info model); 
-	
-	
 	
 	
 	
