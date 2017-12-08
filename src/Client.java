4bd5d9346e3fdf8499f617f11131c91245d5fe5f
Brandon Archbold <babrandon21@gmail.com>
2017-12-08 14:33:09 -0800

    [WIP]initial networking code

---
+++ src/Client.java
@@ -0,0 +1,11 @@
+import java.net.Socket;
+
+public class Client extends Thread {
+
+	Socket socket;
+	
+	public Client() {
+		
+	}
+
+}
---
+++ src/Server.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.net.*;
+
+public class Server extends Thread {
+
+	ServerSocket serverSocket = null;
+	
+	public Server(){
+		try{
+			serverSocket = new ServerSocket(39587);
+		}catch (IOException e){
+			System.out.println("Could not listen on port 39587");
+		}
+	}
+	
+	public Server(int port){
+		try{
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e){
+			System.out.println("Could not listen on port " + port);
+		}
+	}
+	
+	public void run(){
+		while(true){
+			Client clientThread;
+			Socket client;
+			try{
+				client = serverSocket.accept();
+				clientThread = new Client(client);
+				clientThread.start();
+				System.out.println("Connection established from " + client.getInetAddress());
+			} catch (IOException e){
+				System.out.println("Connection failed!");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
