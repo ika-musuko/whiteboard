18f4fee148b3d9afcebae71240673fe797b2353c
brandon-a <babrandon21@gmail.com>
2017-12-08 21:43:09 -0800

    fixed errors in rectangleUtils

--- src/RectangleUtils.java
+++ src/RectangleUtils.java
@@ -2,12 +2,12 @@ import java.awt.*;
 import java.util.*;
 
 public class RectangleUtils {
-    public static List<Point> rectPoints(Rectangle r){
-        return new ArrayList<Point>(
-                                     new Point(r.getX()             , r.getY()              )
-                                    ,new Point(r.getX()+r.getWidth(), r.getY()              )
-                                    ,new Point(r.getX()             , r.getY()+r.getHeight())
-                                    ,new Point(r.getX()+r.getWidth(), r.getY()+r.getHeight())
-                                   );
+    public static ArrayList<Point> rectPoints(Rectangle r){
+        ArrayList<Point> list = new ArrayList<Point>();
+        list.add(new Point(r.x, r.y));
+        list.add(new Point((int) (r.x+r.getWidth()), r.y));
+        list.add(new Point(r.x, (int) (r.y+r.getHeight())));
+        list.add(new Point((int) (r.x+r.getWidth()),(int)( r.y+r.getHeight())));
+        return list;
     }
 }
