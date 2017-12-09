08202ce50842b0a93306689216579b893286d717
sherwyn <sherwyn.sen@gmail.com>
2017-12-08 21:56:59 -0800

    use int fields instead of double methods, no typecasting

--- src/RectangleUtils.java
+++ src/RectangleUtils.java
@@ -5,9 +5,9 @@ public class RectangleUtils {
     public static ArrayList<Point> rectPoints(Rectangle r){
         ArrayList<Point> list = new ArrayList<Point>();
         list.add(new Point(r.x, r.y));
-        list.add(new Point((int) (r.x+r.getWidth()), r.y));
-        list.add(new Point(r.x, (int) (r.y+r.getHeight())));
-        list.add(new Point((int) (r.x+r.getWidth()),(int)( r.y+r.getHeight())));
+        list.add(new Point(r.x+r.width, r.y));
+        list.add(new Point(r.x, r.y+r.height));
+        list.add(new Point(r.x+r.width,r.y+r.height));
         return list;
     }
 }
